101dc5c37bfe71c46b7b6a2f5788cd8c439520fc
swmoon <swmoon@bluedigm.com>
2018-02-05 13:50:04 +0900

    PAAS-TA API 업그레이드 작업 진행중 - V2 통합작업 진행중 - Mapper 업데이트 작업 진행중 - UI 오류 수정중

--- src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
+++ src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
@@ -2,7 +2,6 @@ package org.openpaas.paasta.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * The type Application.
