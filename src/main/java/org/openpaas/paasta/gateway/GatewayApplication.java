6e32556a7a1cddbb068e0aa4e7292c0237fdc1e4
swmoon <swmoon@bluedigm.com>
2018-02-06 11:22:26 +0900

    PAAS-TA V2 - build.gradle 업데이트

--- src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
+++ src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
@@ -1,7 +1,9 @@
 package org.openpaas.paasta.gateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * The type Application.
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.openpaas.paasta.portal.gateway"})
 public class GatewayApplication {
 
     public static void main(String[] args) {
