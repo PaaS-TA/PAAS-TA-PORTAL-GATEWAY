7773ac530685f8a7fe1c3bde3069f8564f598db0
swmoon <swmoon@bluedigm.com>
2018-03-06 15:55:40 +0900

    build gradle 잘못된 Merge로 인한 버그 수정

--- src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
+++ src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
@@ -1,6 +1,9 @@
 package org.openpaas.paasta.gateway;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * The type Application.
  */
