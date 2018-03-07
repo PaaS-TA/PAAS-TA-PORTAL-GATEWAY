2103098b5a360b561f295d8aec51f6d3a4773df3
swmoon <swmoon@bluedigm.com>
2018-03-07 13:44:29 +0900

    PortalGateWay - 정상적으로 구동안되는 문제 수정

--- src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
+++ src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
@@ -3,12 +3,13 @@ package org.openpaas.paasta.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 /**
  * The type Application.
  */
 
-
+@EnableZuulServer
 @SpringBootApplication
 public class GatewayApplication {
 
