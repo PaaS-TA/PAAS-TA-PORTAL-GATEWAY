f8fe92b8f27103018188b72a6e7dbd3b71e23170
swmoon <swmoon@bluedigm.com>
2018-03-15 14:21:38 +0900

    PortalGateWay - Config 수정 - Eureka 문제 수정

--- src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
+++ src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
@@ -3,14 +3,15 @@ package org.openpaas.paasta.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 /**
  * The type Application.
  */
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
