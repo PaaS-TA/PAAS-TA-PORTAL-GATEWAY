8e55ced3f491bb49dbdd6cc6c8e8e16658417ed1
swmoon <swmoon@bluedigm.com>
2018-04-12 16:38:08 +0900

    긱허브 테스트

--- src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
+++ src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
@@ -14,10 +14,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
 }
 
