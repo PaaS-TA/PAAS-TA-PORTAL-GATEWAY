27867b52430f2911e3955491012a4984b10affbb
박철한 <parkcheolhan@bluedigm.com>
2019-03-21 10:41:16 +0900

    branch 테스트

--- src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
+++ src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
@@ -17,6 +17,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+
     }
 }
 
